f4c2bfe7043d26a83dbc3781ef7cebbbe5d3dc13
Leonardo Ferreira <leonardo.gini.96@hotmail.com>
2024-05-11 22:15:53 -0300

    #002 - Implementação Tela Candidatos Por Estado

---
+++ js/relatorio.js
@@ -0,0 +1,45 @@
+function gerarRelatorio(Url) {
+    fetch(Url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        exibirRelatorio(data);
+    })
+    .catch(error => {
+        console.error('Erro ao gerar relatório:', error);
+    });
+}
+
+function exibirRelatorio(data) {
+    var relatorioDiv = document.getElementById("relatorio");
+    relatorioDiv.innerHTML = '';
+
+    if (data.length === 0) {
+        relatorioDiv.innerHTML = 'Nenhum pedido encontrado.';
+        return;
+    }
+
+    var table = document.createElement('table');
+    table.classList.add('tabela'); 
+    var headerRow = table.insertRow(0);
+
+    Object.keys(data[0]).forEach(function(key) {
+        var th = document.createElement('th');
+        th.textContent = key;
+        headerRow.appendChild(th);
+    });
+
+    data.forEach(function(rowData) {
+        var row = table.insertRow(-1);
+        Object.values(rowData).forEach(function(value) {
+            var cell = row.insertCell(-1);
+            cell.textContent = value;
+        });
+    });
+
+    relatorioDiv.appendChild(table);
+}
